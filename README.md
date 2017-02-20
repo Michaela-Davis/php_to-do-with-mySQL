2f9b78e1e84afd66fb5cf8215011e3391778b36d
Michaela-Davis <michaela.delight@gmail.com>
2017-02-19 17:02:41 -0800

    init commit, completes MySQL 1 homework, all tests pass

---
+++ README.md
@@ -0,0 +1,33 @@
+# _Tasks List Webpage_
+
+#### _A webpage that lists tasks, 19 February 2017_
+
+#### By _**Michaela Davis**_
+
+## Description
+
+_A webpage that allows a user to enter a task to be completed, view tasks list, and clear the list._
+
+
+## Known Bugs
+
+_None so far._
+
+## Support and contact details
+
+_Please contact michaela.delight@gmail.com with concerns or comments._
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _PHP_
+* _Silex_
+* _Twig_
+* _mySQL_
+
+### License
+
+*MIT license*
+
+Copyright (c) 2017 **_Michaela Davis_**
---
+++ tests/TaskTest.php
@@ -0,0 +1,73 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Tasks.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class TasksTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Tasks::deleteAll();
+        }
+
+        function test_save()
+        {
+            ///   Arrange   ///
+            $description = "Wash the dog";
+            $test_task = new Tasks($description);
+
+            ///   Act   ///
+            $test_task->save();
+
+            ///   Assert   ///
+            $result = Tasks::getAll();
+            $this->assertEquals($test_task, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            ///   Arrange   ///
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_task = new Tasks($description);
+            $test_task->save();
+            $test_task2 = new Tasks($description2);
+            $test_task2->save();
+
+            ///   Act   ///
+            $result = Tasks::getAll();
+
+            ///   Assert   ///
+            $this->assertEquals([$test_task, $test_task2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            ///   Arrange   ///
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_task = new Tasks($description);
+            $test_task->save();
+            $test_task2 = new Tasks($description2);
+            $test_task2->save();
+
+            ///   Act   ///
+            Tasks::deleteAll();
+
+            ///   Assert   ///
+            $result = Tasks::getAll();
+            $this->assertEquals([], $result);
+        }
+    }
+
+?>
